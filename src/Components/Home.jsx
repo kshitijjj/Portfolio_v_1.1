f788dd2e88eaede5d5f03823caff2a00941c012b
Mayank <myank07official@gmail.com>
2024-09-17 19:29:14 +0530

    updated home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
         <div className='py-[2rem] md:pt-[2.8rem]'>
             {/* Heading */}
             <div className=' pl-[1.5rem] mt-[1.5rem] pb-[0.6rem] md:px-[5rem] md:pb-[1.5rem] md:mt-[2rem]'>
-                <p className='text-[1.26rem] md:w-[50rem] md:text-[2.5rem] opacity-80  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>SOFTWARE <span  className="text-[1.26rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
-                <p className='text-[1.26rem] md:w-[50rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>DATA ANALYSIS</p>
-                <p className='text-[1.26rem] md:w-[60rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>MERN STACK <span className="text-[1.26rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
+                <p className='text-[1.26rem] md:w-[50rem] md:text-[2.5rem] opacity-80  leading-[2.1rem]  md:leading-[3.5rem] md:font-montserrat font-black'>SOFTWARE <span  className="text-[1.26rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
+                <p className='text-[1.26rem] md:w-[50rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] md:font-montserrat font-black'>DATA ANALYSIS</p>
+                <p className='text-[1.26rem] md:w-[60rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] md:font-montserrat font-black'>MERN STACK <span className="text-[1.26rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
             </div>          {/* line */}
            
             <div className=" w-[20rem] md:w-[22rem] ml-[1.6rem] h-[0.02rem] bg-black bg-opacity-30 md:mx-[5rem] md:h-[0.1rem] md:bg-black">
@@ -19,8 +19,8 @@ const Home = () => {
 
             {/* Sub Heading */}
             <div className=' pl-[1.6rem] w-[25rem] pt-[1rem] md:pl-[5rem] md:pt-[1.6rem] md:w-[45rem] '>
-                <p className=' text-[0.75rem] md:text-[1rem] font-montserrat tracking-tight opacity-65'>Hey! my name is Mayank Uppal</p>
-                <p className=" text-[0.75rem] md:text-[1rem] font-montserrat tracking-tight opacity-65">I am a final year undergraduate student at PEC, Chandigarh persuing B.Tech in Electrical Engineering</p>
+                <p className=' text-[0.75rem] md:text-[1rem] md:font-montserrat tracking-tight opacity-65'>Hey! my name is Mayank Uppal</p>
+                <p className=" text-[0.75rem] md:text-[1rem] md:font-montserrat tracking-tight opacity-65">I am a final year undergraduate student at PEC, Chandigarh persuing B.Tech in Electrical Engineering</p>
             </div>
 
             {/* Button */}
