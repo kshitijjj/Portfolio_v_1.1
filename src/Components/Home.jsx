2cf71dca0fc1d14f040f2247133395f2d2481f15
Mayank <myank07official@gmail.com>
2024-09-17 19:13:28 +0530

    updating home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
         <div className='py-[2rem] md:pt-[2.8rem]'>
             {/* Heading */}
             <div className=' pl-[1.5rem] md:px-[5rem] pb-[1.5rem] mt-[2rem]'>
-                <p className='text-[1.4rem] md:w-[50rem] md:text-[2.5rem] opacity-80  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>SOFTWARE <span  className="text-[1.4rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
-                <p className='text-[1.4rem] md:w-[50rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>DATA ANALYSIS</p>
-                <p className='text-[1.4rem] md:w-[60rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>MERN STACK <span className="text-[1.4rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
+                <p className='text-[1.2rem] md:w-[50rem] md:text-[2.5rem] opacity-80  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>SOFTWARE <span  className="text-[1.2rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
+                <p className='text-[1.2rem] md:w-[50rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>DATA ANALYSIS</p>
+                <p className='text-[1.2rem] md:w-[60rem] opacity-80 md:text-[2.5rem]  leading-[2.1rem]  md:leading-[3.5rem] font-montserrat font-black'>MERN STACK <span className="text-[1.2rem] md:text-[2.5rem] font-montserrat font-black opacity-30">DEVELOPMENT</span></p>
             </div>
           {/* line */}
             <div className="w-[22rem] ml-[1.6rem] md:mx-[5rem]  h-[0.1rem] bg-black">
