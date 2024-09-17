b5becd40ad34b2b6a6d735015e8a0c87a5f2bff2
Mayank <myank07official@gmail.com>
2024-09-17 21:03:00 +0530

    updated project card

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 const ProjectCard = (props) => {
     return (
-        <div className='bg-[#EAE7E7] bg-opacity-35 md:p-6 p-8 w-[45rem] rounded-[1rem] ml-[4rem] border-2 mb-[5rem]'> 
+        <div className='bg-[#EAE7E7] bg-opacity-35 md:p-6 p-8 w-[45rem] rounded-[1rem] md:ml-[4rem] border-2 mb-[5rem]'> 
             
             {/* Inner object div */}
-            <div className='flex flex-col md:flex-row md:justify-center md:items-center'>
+            <div className='flex flex-col md:flex-row justify-center items-center'>
 
                 {/* Image */}
-                <div className='bg-white rounded-[1.6rem] justify-center items-center md:w-[25rem]'>
-                    <img className='rounded-[0.8rem] border-2 border-white' src={props.mainimg} alt="" srcset="" />
+                <div className='bg-white rounded-[1.6rem] justify-center items-center w-[18rem] md:w-[25rem]'>
+                    <img className='w-full rounded-[0.8rem] border-2 border-white' src={props.mainimg} alt="" srcset="" />
                 </div>
 
                 {/* Text */}
-                <div className='md:p-6 md:pl-[2.5rem] md:w-[35rem]'>
+                <div className='pt-2 pl-1 text-center  md:p-6 md:text-left md:pl-[2.5rem] md:w-[35rem] w-[25rem]'>
                     <p className='opacity-80 font-montserrat  font-extrabold text-2xl tracking-tight '>{props.projectname}</p>
                     <p className='font-montserrat text-black font-medium opacity-50 text-[0.8rem]  tracking-tight '>{props.projecttech}</p>
                     <p className='font-montserrat opacity-65 font-normal text-[0.8rem] pt-[0.8rem] tracking-tight'>{props.projectdesc}</p>
-                    <div className='flex gap-8 mt-[1.5rem]'>
+                    <div className='flex gap-8 mt-[1.5rem] md:justify-start justify-center'>
                     <button className=" items-center justify-center text-center bg-[#2229D8] hover:bg-[#2c32db] opacity-75 font-bold text-[1rem]text-center px-[1.8rem] py-[0.5rem] rounded-[0.25rem] text-white">{props.buttonname} <span><img className="inline-block mb-[0.2rem] ml-[0.5rem]" src="./github (1).svg"  alt="" /></span></button>
                     <button className=" border-2  border-[#2229D8] text-[#2229D8]  opacity-75 font-bold text-[1rem] text-center px-[2rem] py-[0.4rem] rounded-[0.2rem]">{props.buttonname2} <span><img className=" inline-block w-100% h-6" src={props.buttonimg2} alt="" /></span></button>
                     </div>
