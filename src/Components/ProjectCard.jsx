9849b1bf7ca79768d5bee9cd28f07d4dcefc3549
Mayank <myank07official@gmail.com>
2024-09-18 11:58:19 +0530

    updated project card

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ProjectCard = (props) => {
     return (
-        <div className='bg-[#EAE7E7] bg-opacity-30 md:bg-opacity-35 md:p-6  w-[45rem] rounded-[1rem] md:ml-[4rem] border-2 '> 
+        <div className=' md:bg-[#EAE7E7] bg-opacity-30 md:bg-opacity-35 md:p-6  w-[45rem] rounded-[1rem] md:ml-[4rem] border-2 '> 
             
             {/* Inner object div */}
             <div className='flex flex-col md:flex-row justify-center items-center md:p-0 pl-3 pr-3 pt-3 pb-1'>
 
                 {/* Image */}
-                <div className='bg-white rounded-[1.6rem] justify-center items-center md:mt-0 mt-4 md:w-[25rem] w-[15rem]'>
+                <div className='md:bg-white rounded-[1.6rem] justify-center items-center md:mt-0 mt-4 md:w-[25rem] w-[15rem]'>
                     <img className=' rounded-[0.8rem] border-2 border-white' src={props.mainimg} alt="" srcset="" />
                 </div>
 
